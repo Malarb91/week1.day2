75e6cf106e9baed3b48a973ac50e192f5b8e6781
Malarb91 <96664381+Malarb91@users.noreply.github.com>
2022-01-08 00:42:03 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class Calculator {
+	
+	private int addnum(int a,int b) {
+		return a+b;
+	}
+
+	public static void main(String[] args) {
+		Calculator problems = new Calculator();
+		System.out.println(problems.addnum(12, 13));;
+		
+	}
+
+}
