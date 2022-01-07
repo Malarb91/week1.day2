fec6eb1e89576057838d1aeb6c4ef2cc25352d1b
Malarb91 <96664381+Malarb91@users.noreply.github.com>
2022-01-08 00:41:46 +0530

    Add files via upload

---
+++ Armstrongnumber.java
@@ -0,0 +1,14 @@
+package week1.day2;
+
+public class Armstrongnumber {
+
+	public static void main(String[] args) {
+		// it is a number that is == sum of cubes of its digits
+		//153,370 ,371 and 407
+		
+		int number = 200;
+		
+
+	}
+
+}
